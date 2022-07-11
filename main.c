8e01a410eea76b51645397d248141c6cf735d3cf
Antoine RECHAIN <anrechai@e1r3p7.42.fr>
2022-07-11 17:20:52 +0200

    save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anrechai <anrechai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 19:52:35 by anrechai          #+#    #+#             */
-/*   Updated: 2022/07/07 20:30:27 by anrechai         ###   ########.fr       */
+/*   Updated: 2022/07/11 16:20:57 by anrechai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_check_arg2(char **argv)
 	{
 		j = 0;
 		if (argv[i][0] == '\0')
-				return (-1);
+			return (-1);
 		while (argv[i][j])
 		{
 			if (argv[i][j] < '0' || argv[i][j] > '9')
@@ -39,18 +39,32 @@ int	ft_check_arg(int argc, char **argv)
 	if (argc != 5 && argc != 6)
 	{
 		write(2, "Invalid number of arguments\n", 28);
-			return (-1);
+		return (-1);
 	}
 	if (ft_check_arg2(argv) == -1)
 	{
 		write(2, "Invalid argument\n", 17);
-			return (-1);
+		return (-1);
 	}
 	return (0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
+	long int		time;
+	struct timeval	current_time;
+
+	if (gettimeofday(&current_time, NULL) == -1)
+		return (-1);
+	time = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
+	printf("TIME : %ld\n", time);
+	// printf("TIME : %ld\n", current_time.tv_usec);
+	// printf("TIME : %ld\n", current_time.tv_usec);
+	// printf("TIME : %ld\n", current_time.tv_usec);
+	// printf("TIME : %ld\n", current_time.tv_usec);
+	// printf("seconds : %ld\nmicro seconds : %ld",
+			// current_time.tv_sec,
+			// current_time.tv_usec);
 	if (ft_check_arg(argc, argv) == -1)
 		return (-1);
 	return (0);
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: anrechai <anrechai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 18:29:58 by anrechai          #+#    #+#             */
-/*   Updated: 2022/07/07 20:30:47 by anrechai         ###   ########.fr       */
+/*   Updated: 2022/07/11 16:03:48 by anrechai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 int	ft_check_arg(int argc, char **argv);
 int	ft_check_arg2(char **argv);
